c2e17b4cf4837d71692fb46d25d56ff92c6019e6
Kelley Stephens <kelley.stephens@outlook.com>
2014-07-07 14:33:56 -0500

    cheers part 2

--- cheers.rb
+++ cheers.rb
@@ -9,17 +9,30 @@
 #
 # Note: the “a” vs. “an”
 
+require 'date'
+
 puts "What's your name?"
-name = gets.chomp.upcase
+name = gets.chomp
 if name.empty?
   puts "You must enter a name!"
   exit
 end
 
 name.each_char do |char|
-  vowels = "AEFHILMNORSUX"
+  vowels = "aefhilmnorsux"
   letter = char
   article = (vowels.include? letter) ? "an" : "a"
-  puts "Give me #{article}... #{char}!"
+  puts "Give me #{article}... #{char.upcase}!"
   end
-puts "#{name}'s just GRAND!"
+puts "#{name.upcase}'s just GRAND!"
+
+puts "Hey, #{name.capitalize}, what's your birthday?"
+
+bday = Date.parse(gets.chomp)
+this_year = Date.new(Date.today.year,   bday.month, bday.day )
+if this_year > Date.today
+  days = (this_year - Date.today).to_i
+else
+  days = (Date.new(Date.today.year + 1,   bday.month, bday.day ) - Date.today).to_i
+end
+puts "Awesome! Your birthday is in #{days} days! Happy Birthday in advance!"
