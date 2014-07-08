d2794814b4f86cbecafd034ae768bb1adb2b5e22
Kelley Stephens <kelley.stephens@outlook.com>
2014-07-08 09:30:59 -0500

    updated details

--- cheers.rb
+++ cheers.rb
@@ -1,14 +1,3 @@
-# Build on the results of our in-class exploration to output, for example:
-#
-# Give me an... A
-# Give me a... B
-# Give me a... B
-# Give me a... Y
-# ABBY’s just GRAND!
-# When given the input of “Abby”.
-#
-# Note: the “a” vs. “an”
-
 require 'date'
 
 puts "What's your name?"
@@ -18,11 +7,10 @@ if name.empty?
   exit
 end
 
-name.each_char do |char|
+name.each_char do |letter|
   vowels = "aefhilmnorsux"
-  letter = char
   article = (vowels.include? letter) ? "an" : "a"
-  puts "Give me #{article}... #{char.upcase}!"
+  puts "Give me #{article}... #{letter.upcase}!"
   end
 puts "#{name.upcase}'s just GRAND!"
 
